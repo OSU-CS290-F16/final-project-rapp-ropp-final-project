8202a2f7ad5489927cedcf14a36e78735f99d3ba
roppjo <roppjo@oregonstate.edu>
2016-12-07 11:54:49 -0800

    Add files via upload
    Add index.js, Basically done with html/css.

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 var path = require('path');
 var express = require('express');
-//var exphbs = require('express-handlebars');
+var exphbs = require('express-handlebars');
 
 var app = express();
 //var usersData = require('./package');
 var port = process.env.PORT || 3000;
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/',function(req,res){
-  res.sendFile('index.html');
+  res.render('index.html');
 });
 
-app.get('/',function(req,res){
-	res.sendFile('style.css');
-});
+
+
 
 /* app.use(function(req, res, next){
     res.status(404).render('404');
