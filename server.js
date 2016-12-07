383b165965e1578de14582d5339615900e13c5fe
roppjo <roppjo@oregonstate.edu>
2016-12-07 14:36:30 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 
 var app = express();
-//var usersData = require('./package');
+//var board-data = require('./board-data');
 var port = process.env.PORT || 3000;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.get('/',function(req,res){
   res.render('index-page');
@@ -18,7 +17,6 @@ app.get('/',function(req,res){
 app.get('*', function (req, res, next){
     res.status(404).render('404-page');
 }); 
-
 
 app.listen(port, function () {
   console.log("== Listening on port", port);
