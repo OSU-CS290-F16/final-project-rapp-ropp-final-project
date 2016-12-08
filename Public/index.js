43d253375082a5c395dfddaf229e24ef58137f8b
RyanRapp14 <cubsfanrapp14@yahoo.com>
2016-12-07 16:09:26 -0800

    Update index.js
    Makes the game know who won and alerts ze players

--- Public/index.js
+++ Public/index.js
@@ -26,6 +26,7 @@ function clickSpace(event){
                 otherPlayer = temp;
                 changeHTML(id,one);
                 changeTurn(player);
+                checkWin();
             }else if(id=='two' && two == ' '){
                 two = player;
                 var temp = player;
@@ -33,6 +34,7 @@ function clickSpace(event){
                 otherPlayer = temp;
                 changeHTML(id,two);
                 changeTurn(player);
+                checkWin();
             }else if(id=='three' && three == ' '){
                 three = player;
                 var temp = player;
@@ -40,6 +42,7 @@ function clickSpace(event){
                 otherPlayer = temp;
                 changeHTML(id,three);
                 changeTurn(player);
+                checkWin();
             }else if(id=='four' && four == ' '){
                 four = player;
                 var temp = player;
@@ -47,6 +50,7 @@ function clickSpace(event){
                 otherPlayer = temp;
                 changeHTML(id,four);
                 changeTurn(player);
+                checkWin();
             }else if(id=='five' && five == ' '){
                 five = player;
                 var temp = player;
@@ -54,6 +58,7 @@ function clickSpace(event){
                 otherPlayer = temp;
                 changeHTML(id,five);
                 changeTurn(player);
+                checkWin();
             }else if(id=='six' && six == ' '){
                 six = player;
                 var temp = player;
@@ -61,6 +66,7 @@ function clickSpace(event){
                 otherPlayer = temp;
                 changeHTML(id,six);
                 changeTurn(player);
+                checkWin();
             }else if(id=='seven' && seven == ' '){
                 seven = player;
                 var temp = player;
@@ -68,6 +74,7 @@ function clickSpace(event){
                 otherPlayer = temp;
                 changeHTML(id,seven);
                 changeTurn(player);
+                checkWin();
             }else if(id=='eight' && eight == ' '){
                 eight = player;
                 var temp = player;
@@ -75,6 +82,7 @@ function clickSpace(event){
                 otherPlayer = temp;
                 changeHTML(id,eight);
                 changeTurn(player);
+                checkWin();
             }else if(id=='nine' && nine == ' '){
                 nine = player;
                 var temp = player;
@@ -82,6 +90,7 @@ function clickSpace(event){
                 otherPlayer = temp;
                 changeHTML(id,nine);
                 changeTurn(player);
+                checkWin();
             }
 
 
@@ -89,39 +98,49 @@ function clickSpace(event){
 
 
 checkWin = function () { // CHECKS IF X WON
-    if (("one" == "four" && "one" == "seven" && ("one" == "x")) || //first column
-    ("two" == "five" && "two" == "eight" && ("two" == "x")) || //second column
-    ("three" == "six" && "three" == "nine" && ("three" == "x")) || //third column
-    ("one" == "two" && "one" == "three" && ("one" == "x")) || //first row
-    ("four" == "five" && "four" == "six" && ("four" == "x")) || //second row
-    ("seven" == "eight" && "seven" == "nine" && ("seven" == "x")) || //third row
-    ("one" == "five" && "one" == "nine" && ("one" == "x")) || //diagonal 1
-    ("seven" == "five" && "seven" == "three" && ("seven" == "x")) //diagonal 2
+    if ((one == four && one == seven && (one == "x")) || //first column
+    (two == five && two == eight && (two == "x")) || //second column
+    (three == six && three == nine && (three == "x")) || //third column
+    (one == two && one == three && (one == "x")) || //first row
+    (four == five && four == six && (four == "x")) || //second row
+    (seven == eight && seven == nine && (seven == "x")) || //third row
+    (one == five && one == nine && (one == "x")) || //diagonal 1
+    (seven == five && seven == three && (seven == "x")) //diagonal 2
     ) {
         xWin = true;
+        oWin = false;
         winAlert();
 
     } else { // CHECKS IF O WON
-        if (("one" == "four" && "one" == "seven" && ("one" == "o")) || //first column
-        ("two" == "five" && "two" == "eight" && ("two" == "o")) || //second column
-        ("three" == "six" && "three" == "nine" && ("three" == "o")) || //third column
-        ("one" == "two" && "one" == "three" && ("one" == "o")) || //first row
-        ("four" == "five" && "four" == "six" && ("four" == "o")) || //second row
-        ("seven" == "eight" && "seven" == "nine" && ("seven" == "o")) || //third row
-        ("one" == "five" && "one" == "nine" && ("one" == "o")) || //diagonal 1
-        ("seven" == "five" && "seven" == "three" && ("seven" == "o")) //diagonal 2
+        if ((one == four && one == seven && (one == "o")) || //first column
+        (two == five && two == eight && (two == "o")) || //second column
+        (three == six && three == nine && (three == "o")) || //third column
+        (one == two && one == three && (one == "o")) || //first row
+        (four == five && four == six && (four == "o")) || //second row
+        (seven == eight && seven == nine && (seven == "o")) || //third row
+        (one == five && one == nine && (one == "o")) || //diagonal 1
+        (seven == five && seven == three && (seven == "o")) //diagonal 2
         ) {
             oWin = true;
+            xWin = false;
             winAlert();
 
-        } else { 
-            if ((("one" == "x") || ("one" == "o")) && (("two" == "x") || ("two" == "o")) && (("three" == "x") || ("three" == "o")) && (("four" == "x") || ("four" == "o")) && (("five" == "x") || ("five" == "o")) && (("six" == "x") || ("six" == "o")) && (("seven" == "x") || ("seven" == "o")) && (("eight" == "x") || ("eight" == "o")) && (("nine" == "x") || ("nine" == "o"))) {
+        } else {
+            //if ((("one" == "x") || ("one" == "o")) && (("two" == "x") || ("two" == "o")) && (("three" == "x") || ("three" == "o")) && (("four" == "x") || ("four" == "o")) && (("five" == "x") || ("five" == "o")) && (("six" == "x") || ("six" == "o")) && (("seven" == "x") || ("seven" == "o")) && (("eight" == "x") || ("eight" == "o")) && (("nine" == "x") || ("nine" == "o"))) {
+            if(one != " " && two != " " && three != " " && four != " " && five != " " && six != " " && seven != " " && eight != " " && nine != " "){
                 alert("It's a tie!");
             }
         }
     }
 };
 
+function winAlert(){
+    if(xWin){
+        alert("X Wins!!!!!!!");
+    }else if(oWin){
+        alert("O Wins!!!!!!!")
+    }
+}
 function changeHTML(id,value){
 
     document.getElementById(id).innerHTML = value;
@@ -134,6 +153,5 @@ function changeHTML(id,value){
   for (var i = 0; i < box.length; i++) {
 	box[i].addEventListener('click', clickSpace)
   }
-
 
 
